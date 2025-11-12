f2e0f88668b643bbeefe80fd0079998bf8d0d242
Ghislain Haine <ghislain.haine@isae.fr>
2025-11-12 15:23:53 +0100

    getData OK?

--- docs/getData.sh
+++ docs/getData.sh
@@ -48,143 +48,143 @@ first=true
 # Boucle sur les DOIs
 k=0
 while IFS= read -r doi; do
-    (( ++k )) || :
-    echo $(date -Iseconds)" DOI $k: $doi"
-    # Extraction des données
-    response=$(fetch_metadata_crossref "$doi")
-    status=$(echo "$response" | jq -r '.status')
+  (( ++k )) || :
+  log $(date -Iseconds)" DOI $k: $doi"
+  # Extraction des données
+  response=$(fetch_metadata_crossref "$doi")
+  status=$(read_json "$response" '.status')
 
-    if [[ "$status" == "ok" ]]; then
-        # Récupère les data
-        title=$(echo "$response" | jq -r '.message.title // [""] | .[0]' | sed -E 's/<[^>]*mml[^>]*>//g' | sed -E 's/"/\\"/g')
-        authors=$(echo "$response" | jq -r '.message.author')
-        type=$(echo "$response" | jq -r '.message.type // ""')
-        abstract=$(echo "$response" | jq -r '.message.abstract // ""')
-        journal=$(echo "$response" | jq -r '.message["container-title"] // [""] | .[0]')
-        year=$(echo "$response" | jq -r '.message["published-print"]["date-parts"][0][0] // ""')
-        volume=$(echo "$response" | jq -r '.message.volume // ""')
-        issue=$(echo "$response" | jq -r '.message.issue // ""')
-        event=""
-        isbn=$(echo "$response" | jq -r '.message["isbn-type"][0]["value"] // ""')
-        pages=$(echo "$response" | jq -r '.message.page // "" | gsub("-"; "--")')
-        publisher=$(echo "$response" | jq -r '.message.publisher // ""')
-        keywords=$(echo "$response" | jq -r '.message.subject // [] | join(", ")')
-        dateY=$(echo "$response" | jq -r '.message.created["date-parts"][0][0] // ""')
-        dateM=$(echo "$response" | jq -r '.message.created["date-parts"][0][1] // ""')
-        dateD=$(echo "$response" | jq -r '.message.created["date-parts"][0][2] // ""')
-        
-        # Génère le slug, vérifie son unicité grâce aux bib existants, puis génère le .bib associé
-        slug=$(slugify "$title")
-        bibfile="assets/bib/$slug.bib"
-        while [ -f $bibfile ]; do
-            slug+="0"
-            bibfile="assets/bib/$slug.bib"
-        done
-        $(print_bib "$doi" "$bibfile")
-        
-        # Formate les références
-        references="["
-        while IFS= read -r ref; do
-            d_ref=$(echo "$ref" | jq -r .DOI 2>/dev/null)
-            if echo "$d_ref" | grep -q "10."; then
-                ti_ref=$(get_citation "$d_ref" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g')
-            else
-                ti_ref=$(echo "$ref" | jq -r '
-                    [
-                        (if .author then .author + "," else empty end),
-                        (if .["article-title"] then .["article-title"] + "." else empty end),
-                        (if .["journal-title"] then .["journal-title"] else empty end),
-                        (if .["volume-title"] then .["volume-title"] else empty end),
-                        (if .year then "(" + (.year|tostring) + ")" else empty end)
-                    ] | map(select(. != "")) | join(" ")' | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g')
-            fi
-            references+='{"doi": "'$d_ref'", "title": "'$ti_ref'"},'
-        done < <(echo "$response" | jq -c '.message.reference[]' 2>/dev/null)
-        if [ "$references" != "[" ]; then
-            references="${references::-1}"
-        fi
-        references+="]"
-        
-        # Year from creation if not published-print
-        if [ -z "$year" ]; then
-            year=$(echo $dateY)
-        fi
-        
-        # Récupère l'url "final" (avant redirection js) à partir du DOI
-        url=$(curl -Ls -o /dev/null -w "%{url_effective}" "https://doi.org/$doi")
-        
-        # Update si elsevier
-        if echo "$url" | grep -q "elsevier"; then
-            json_scopus=$(fetch_metadata_scopus "$doi")
-            abstract=$(abstract_from_scopus "$json_scopus")
-            keywords=$(keywords_from_scopus "$json_scopus")
-            event=$(event_from_scopus "$json_scopus")
-        fi
-        
-        # Update si springer
-        if echo "$url" | grep -q "springer"; then
-            json_springer=$(fetch_metadata_springer "$doi")
-            abstract=$(abstract_from_springer "$json_springer")
-            keywords=$(keywords_from_springer "$json_springer")
-            event=$(event_from_springer "$json_springer")
-        fi
-        
-        # Update si ieee
-        if echo "$url" | grep -q "ieee"; then
-            json_ieee=$(fetch_metadata_ieee "$doi")
-            abstract=$(abstract_from_ieee "$json_ieee")
-            keywords=$(keywords_from_ieee "$json_ieee")
-            event=$(event_from_ieee "$json_ieee")
-        fi
-        
-        # Complement pour l'abstract
-        if [ -z "$abstract" ]; then
-            complement=$(fetch_abstract_complement "$doi")
-            [ -n "$complement" ] && abstract="$complement"
-        fi
-
-        # Nettoyage de l'abstract, des keywords et de l'event
-        abstract=$(echo "$abstract" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g' | sed -E 's/summary//Ig' | sed -E 's/abstract//Ig')
-        keywords=$(echo "$keywords" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g')
-        event=$(echo "$event" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g')
-            
-        if [ "$first" = true ]; then
-            first=false || :
+  if [[ "$status" == "ok" ]]; then
+    # Récupère les data
+    title=$(read_json "$response" '.message.title // [""] | .[0]' | sed -E 's/<[^>]*mml[^>]*>//g' | sed -E 's/"/\\"/g')
+    authors=$(read_json "$response" '.message.author')
+    type=$(read_json "$response" '.message.type // ""')
+    abstract=$(read_json "$response" '.message.abstract // ""')
+    journal=$(read_json "$response" '.message["container-title"] // [""] | .[0]')
+    year=$(read_json "$response" '.message["published-print"]["date-parts"][0][0] // ""')
+    volume=$(read_json "$response" '.message.volume // ""')
+    issue=$(read_json "$response" '.message.issue // ""')
+    event=""
+    isbn=$(read_json "$response" '.message["isbn-type"][0]["value"] // ""')
+    pages=$(read_json "$response" '.message.page // "" | gsub("-"; "--")')
+    publisher=$(read_json "$response" '.message.publisher // ""')
+    keywords=$(read_json "$response" '.message.subject // [] | join(", ")')
+    dateY=$(read_json "$response" '.message.created["date-parts"][0][0] // ""')
+    dateM=$(read_json "$response" '.message.created["date-parts"][0][1] // ""')
+    dateD=$(read_json "$response" '.message.created["date-parts"][0][2] // ""')
+    
+    # Génère le slug, vérifie son unicité grâce aux bib existants, puis génère le .bib associé
+    slug=$(slugify "$title")
+    bibfile="assets/bib/$slug.bib"
+    while [ -f $bibfile ]; do
+      slug+="0"
+      bibfile="assets/bib/$slug.bib"
+    done
+    $(print_bib "$doi" "$bibfile")
+    
+    # Formate les références
+    references="["
+    while IFS= read -r ref; do
+        d_ref=$(echo "$ref" | jq -r .DOI 2>/dev/null)
+        if echo "$d_ref" | grep -q "10."; then
+          ti_ref=$(get_citation "$d_ref" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g')
         else
-            echo "," >> "$output_json"
+          ti_ref=$(read_json "$ref" '
+              [
+                  (if .author then .author + "," else empty end),
+                  (if .["article-title"] then .["article-title"] + "." else empty end),
+                  (if .["journal-title"] then .["journal-title"] else empty end),
+                  (if .["volume-title"] then .["volume-title"] else empty end),
+                  (if .year then "(" + (.year|tostring) + ")" else empty end)
+              ] | map(select(. != "")) | join(" ")' | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g')
         fi
-
-        # .json format de toutes les datas
-        echo "  {" >> "$output_json"
-        echo "    \"doi\": \"$doi\"," >> "$output_json"
-        echo "    \"type\": \"$type\"," >> "$output_json"
-        echo "    \"title\": \"$title\"," >> "$output_json"
-        echo "    \"authors\": $authors," >> "$output_json"
-        echo "    \"abstract\": \"$abstract\"," >> "$output_json"
-        echo "    \"journal\": \"$journal\"," >> "$output_json"
-        echo "    \"year\": \"$year\"," >> "$output_json"
-        echo "    \"volume\": \"$volume\"," >> "$output_json"
-        echo "    \"issue\": \"$issue\"," >> "$output_json"
-        echo "    \"event\": \"$event\"," >> "$output_json"
-        echo "    \"isbn\": \"$isbn\"," >> "$output_json"
-        echo "    \"pages\": \"$pages\"," >> "$output_json"
-        echo "    \"publisher\": \"$publisher\"," >> "$output_json"
-        echo "    \"keywords\": \"$keywords\"," >> "$output_json"
-        echo "    \"dateY\": \"$dateY\"," >> "$output_json"
-        echo "    \"dateM\": \"$dateM\"," >> "$output_json"
-        echo "    \"dateD\": \"$dateD\"," >> "$output_json"
-        echo "    \"permalink\": \"$slug\"," >> "$output_json"
-        echo "    \"references\": $references" >> "$output_json"
-        echo "  }" >> "$output_json"
-    else
-        echo "DOI: $doi is not available yet."
+        references+='{"doi": "'$d_ref'", "title": "'$ti_ref'"},'
+    done < <(echo "$response" | jq -c '.message.reference[]' 2>/dev/null)
+    if [ "$references" != "[" ]; then
+      references="${references::-1}"
     fi
+    references+="]"
+    
+    # Year from creation if not published-print
+    if [ -z "$year" ]; then
+      year=$(echo $dateY)
+    fi
+    
+    # Récupère l'url "final" (avant redirection js) à partir du DOI
+    url=$(curl -Ls -o /dev/null -w "%{url_effective}" "https://doi.org/$doi")
+    
+    # Update si elsevier
+    if echo "$url" | grep -q "elsevier"; then
+      json_scopus=$(fetch_metadata_scopus "$doi")
+      abstract=$(abstract_from_scopus "$json_scopus")
+      keywords=$(keywords_from_scopus "$json_scopus")
+      event=$(event_from_scopus "$json_scopus")
+    fi
+    
+    # Update si springer
+    if echo "$url" | grep -q "springer"; then
+      json_springer=$(fetch_metadata_springer "$doi")
+      abstract=$(abstract_from_springer "$json_springer")
+      keywords=$(keywords_from_springer "$json_springer")
+      event=$(event_from_springer "$json_springer")
+    fi
+    
+    # Update si ieee
+    if echo "$url" | grep -q "ieee"; then
+      json_ieee=$(fetch_metadata_ieee "$doi")
+      abstract=$(abstract_from_ieee "$json_ieee")
+      keywords=$(keywords_from_ieee "$json_ieee")
+      event=$(event_from_ieee "$json_ieee")
+    fi
+    
+    # Complement pour l'abstract
+    if [ -z "$abstract" ]; then
+      complement=$(fetch_abstract_complement "$doi")
+      [ -n "$complement" ] && abstract="$complement"
+    fi
+
+    # Nettoyage de l'abstract, des keywords et de l'event
+    abstract=$(echo "$abstract" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g' | sed -E 's/summary//Ig' | sed -E 's/abstract//Ig')
+    keywords=$(echo "$keywords" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g')
+    event=$(echo "$event" | tr -d '\000-\031' | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | sed -E 's/\\/\\\\/g' | sed -E 's/"/\\"/g' | sed -E 's/<[^>]*jats[^>]*>//g')
+        
+    if [ "$first" = true ]; then
+      first=false || :
+    else
+      echo "," >> "$output_json"
+    fi
+
+    # .json format de toutes les datas
+    echo "  {" >> "$output_json"
+    echo "    \"doi\": \"$doi\"," >> "$output_json"
+    echo "    \"type\": \"$type\"," >> "$output_json"
+    echo "    \"title\": \"$title\"," >> "$output_json"
+    echo "    \"authors\": $authors," >> "$output_json"
+    echo "    \"abstract\": \"$abstract\"," >> "$output_json"
+    echo "    \"journal\": \"$journal\"," >> "$output_json"
+    echo "    \"year\": \"$year\"," >> "$output_json"
+    echo "    \"volume\": \"$volume\"," >> "$output_json"
+    echo "    \"issue\": \"$issue\"," >> "$output_json"
+    echo "    \"event\": \"$event\"," >> "$output_json"
+    echo "    \"isbn\": \"$isbn\"," >> "$output_json"
+    echo "    \"pages\": \"$pages\"," >> "$output_json"
+    echo "    \"publisher\": \"$publisher\"," >> "$output_json"
+    echo "    \"keywords\": \"$keywords\"," >> "$output_json"
+    echo "    \"dateY\": \"$dateY\"," >> "$output_json"
+    echo "    \"dateM\": \"$dateM\"," >> "$output_json"
+    echo "    \"dateD\": \"$dateD\"," >> "$output_json"
+    echo "    \"permalink\": \"$slug\"," >> "$output_json"
+    echo "    \"references\": $references" >> "$output_json"
+    echo "  }" >> "$output_json"
+  else
+    log " DOI: $doi is not available yet."
+  fi
 
 done < "$input_file"
 
 echo "]" >> "$output_json"
 
 # Tout s'est bien passé !
-echo "Données récupérées avec succès !"
+log $(date -Iseconds)" Data get successfully!"
 exit 0
 
