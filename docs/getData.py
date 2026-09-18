aafe046c564665b0545924e72f3eb282f05b5010
Ghislain HAINE <94449556+g-haine@users.noreply.github.com>
2026-09-18 09:33:10 +0200

    feat: cut getData over to canonical staging

--- docs/getData.py
+++ docs/getData.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Fetch metadata and BibTeX for a file of new DOIs."""
 from phraise_tools.common import parser, run_cli
-from phraise_tools.collect import collect
+from phraise_tools.canonical import collect_canonical
 from phraise_tools.metadata import Client
 
 
@@ -9,7 +9,7 @@ def main(argv=None):
     cli = parser(__doc__)
     cli.add_argument('doi_file', help='DOI file, relative to --root or absolute')
     args = cli.parse_args(argv)
-    return run_cli(lambda a: collect(
+    return run_cli(lambda a: collect_canonical(
         a.root, a.root / a.doi_file,
         Client(a.root, reporter=a.reporter), a.reporter, a.dry_run), args)
 
